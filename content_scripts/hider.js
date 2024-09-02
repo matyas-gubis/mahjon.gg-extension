e1e5603d2e36516282ece3d59497d72b7f201f0f
matyasgubis <gubismatyas@gmail.com>
2024-09-02 17:37:10 +0200

    format hider

--- content_scripts/hider.js
+++ content_scripts/hider.js
@@ -1,32 +1,30 @@
 chrome.runtime.onMessage.addListener((msg) => {
-  if (msg && msg.action === "hideBlocked") {
-    const hiderStyle = document.getElementById("hider-style");
+  if (msg && msg.action === 'hideBlocked') {
+    const hiderStyle = document.getElementById('hider-style');
     if (hiderStyle) {
       hiderStyle.remove();
-      window.removeEventListener("click", hideBlocked, true);
+      window.removeEventListener('click', hideBlocked, true);
     } else {
-      const style = document.createElement("style");
-      style.innerHTML = ".hidden {display: none}";
-      style.id = "hider-style";
+      const style = document.createElement('style');
+      style.innerHTML = '.hidden {display: none}';
+      style.id = 'hider-style';
       document.head.appendChild(style);
 
-      window.addEventListener("click", hideBlocked, true);
+      window.addEventListener('click', hideBlocked, true);
       hideBlocked();
     }
   }
 });
 
 function hideBlocked() {
-  const tiles = document.querySelectorAll(".tileContainer");
-  tiles.forEach((tile) => {
-    if (
-      !Array.from(tile.children).some((child) =>
-        child.className.includes("unblocked")
-      )
-    ) {
-      tile.classList.add("hidden");
-    } else {
-      tile.classList.remove("hidden");
-    }
-  });
+  setTimeout(() => {
+    const tiles = document.querySelectorAll('.tileContainer');
+    tiles.forEach((tile) => {
+      if (!Array.from(tile.children).some((child) => child.className.includes('unblocked'))) {
+        tile.classList.add('hidden');
+      } else {
+        tile.classList.remove('hidden');
+      }
+    });
+  }, 500);
 }
